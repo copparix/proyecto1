b07590ba0b74f2d2aa65d2f3c5b99dd6063d890c
Vale <arivalcoppari@gmail.com>
2023-04-21 19:44:43 -0300

    Agregado loginActivity

--- app/src/main/java/com/example/proyecto1/MainActivity.java
+++ app/src/main/java/com/example/proyecto1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.proyecto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goLogin(View view){
+        Intent intent = new Intent(MainActivity.this, loginActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
 }
